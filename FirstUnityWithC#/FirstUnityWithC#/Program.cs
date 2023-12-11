0d2b6ed40034eab79c6c610244a75536439bb050
nicog33b <nicolasg99dr@gmail.com>
2023-12-11 13:31:55 -0300

    primer ejercicio

---
+++ FirstUnityWithC#/FirstUnityWithC#/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstUnityWithC_
+{
+    internal class Program
+    {
+        private static String Mensaje = "Hola n00b";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mensaje);
+        }
+    }
+}
